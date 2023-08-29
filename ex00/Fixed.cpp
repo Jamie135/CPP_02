bd8436443525122518cbad8fac678f4145b9a99a
Pomprab BUREERAK <pbureera@paul-f4Br1s1.clusters.42paris.fr>
2023-08-29 14:12:22 +0200

    OK

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -1,1 +1,38 @@
 #include "Fixed.hpp"
+
+Fixed::Fixed(): fp_value(0)
+{
+	std::cout << "Default Constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy Constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &src)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &src)
+		this->fp_value = src.getRawBits();
+
+	return *this;
+}
+
+int	Fixed::getRawBits(void)const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->fp_value);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fp_value = raw;
+}
