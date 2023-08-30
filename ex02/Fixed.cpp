e55e46618e8ffe64a917b3caaeeec2e7eb30e7c0
unknown <jamie.gworld@hotmail.com>
2023-08-30 19:09:05 +0700

    OK

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -1,5 +1,6 @@
 #include "Fixed.hpp"
 
+//Constructor/Destructor
 Fixed::Fixed(): fp_value(0)
 {
 	// std::cout << "Default Constructor called" << std::endl;
@@ -28,6 +29,7 @@ Fixed::~Fixed()
 	// std::cout << "Fixed Deconstructor called" << std::endl;
 }
 
+//Equal Operator
 Fixed	&Fixed::operator=(const Fixed &src)
 {
 	// std::cout << "Fixed Assignation operator called" << std::endl;
@@ -37,6 +39,89 @@ Fixed	&Fixed::operator=(const Fixed &src)
 	return *this;
 }
 
+//Comparison Operators
+bool	Fixed::operator>(Fixed n) const
+{
+	return (this->toFloat() > n.toFloat());
+}
+
+bool	Fixed::operator<(Fixed n) const
+{
+	return (this->toFloat() < n.toFloat());
+}
+
+bool	Fixed::operator>=(Fixed n) const
+{
+	return (this->toFloat() >= n.toFloat());
+}
+
+bool	Fixed::operator<=(Fixed n) const
+{
+	return (this->toFloat() <= n.toFloat());
+}
+
+bool	Fixed::operator==(Fixed n) const
+{
+	return (this->toFloat() == n.toFloat());
+}
+
+bool	Fixed::operator!=(Fixed n) const
+{
+	return (this->toFloat() != n.toFloat());
+}
+
+//Arithmetic Operators
+float	Fixed::operator+(Fixed n) const
+{
+	return (this->toFloat() + n.toFloat());
+}
+
+float	Fixed::operator-(Fixed n) const
+{
+	return (this->toFloat() - n.toFloat());
+}
+
+float	Fixed::operator*(Fixed n) const
+{
+	return (this->toFloat() * n.toFloat());
+}
+
+float	Fixed::operator/(Fixed n) const
+{
+	return (this->toFloat() / n.toFloat());
+}
+
+//Pre-increment Operators
+Fixed	Fixed::operator++()
+{
+	this->fp_value++;
+	return (*this);
+}
+
+Fixed	Fixed::operator--()
+{
+	this->fp_value--;
+	return (*this);
+}
+
+//Post-increment Operators
+Fixed	Fixed::operator++(int)
+{
+	Fixed tmp = *this;
+	// i = this->fp_value;
+	++this->fp_value;
+	return (tmp);
+}
+
+Fixed	Fixed::operator--(int)
+{
+	Fixed tmp = *this;
+	// i = this->fp_value;
+	--this->fp_value;
+	return (tmp);
+}
+
+//Public Methods
 float	Fixed::toFloat(void)const
 {
 	return ((float)this->fp_value / (float)(1 << this->fract_bits));
@@ -47,20 +132,55 @@ int	Fixed::toInt(void)const
 	return (this->fp_value >> this->fract_bits);
 }
 
+Fixed	&Fixed::min(Fixed &n1, Fixed &n2)
+{
+	if (n1.toFloat() <= n2.toFloat())
+		return (n1);
+	else
+		return (n2);
+}
+
+const Fixed	&Fixed::min(const Fixed &n1, const Fixed &n2)
+{
+	if (n1.toFloat() <= n2.toFloat())
+		return (n1);
+	else
+		return (n2);
+}
+
+Fixed	&Fixed::max(Fixed &n1, Fixed &n2)
+{
+	if (n1.toFloat() >= n2.toFloat())
+		return (n1);
+	else
+		return (n2);
+}
+
+const Fixed	&Fixed::max(const Fixed &n1, const Fixed &n2)
+{
+	if (n1.toFloat() >= n2.toFloat())
+		return (n1);
+	else
+		return (n2);
+}
+
+//Getter
 int Fixed::getRawBits(void) const
 {
 	// std::cout << "getRawBits member function called" << std::endl;
 	return (this->fp_value);
 }
 
+//Setter
 void Fixed::setRawBits(int const raw)
 {
 	this->fp_value = raw;
 	// std::cout << "setRawBits member function called" << std::endl;
 }
 
-std::ostream	&operator<<(std::ostream &o, Fixed const &fixed)
+//ostream Operator
+std::ostream	&operator<<(std::ostream &o, Fixed const &n)
 {
-	o << fixed.toFloat();
+	o << n.toFloat();
 	return (o);
 }
